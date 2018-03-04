6bc6d3442a332aca65ea2c4069a078a775d7876f
busrauludag <busrauludag13@gmail.com>
2018-03-04 21:56:40 +0300

    animations and transitiyons app started

---
+++ animations-transitions/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h2>Animations and Transitions</h2>
+    <hr>
+    <button class="btn btn-primary" @click="show = !show">
+      Show Alert
+    </button>
+    <transition>
+      <div class="alert alert-info mt-2" v-if="show">
+        This is some Info !
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+</style>
