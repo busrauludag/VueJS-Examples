3c7e6355ddc23d29e479bd4d1683e1ca4cd6609b
busrauludag <busrauludag13@gmail.com>
2018-03-10 15:42:39 +0300

    vue-route folder created

---
+++ vue-route/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="col-12">
+      <h3>Routing</h3>
+      
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+</style>
