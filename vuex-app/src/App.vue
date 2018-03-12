8b5b356f0d880aaeb3abb54488d0f897e801d9f8
busrauludag <busrauludag13@gmail.com>
2018-03-12 23:21:45 +0300

    vuex started

---
+++ vuex-app/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>VUEX</h1>
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Counter from './components/Counter.vueğ*'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@import '../node_modules/bootstrap/dist/css/bootstrap.css';
+</style>
---
+++ vuex-app/src/components/Result.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>VUEX</h1>
+
+      </div>
+    </div>
+  </div>
+</template>
