832dfec386e86690033c45827bacafb690d4afdf
busrauludag <busrauludag13@gmail.com>
2018-03-19 21:22:38 +0300

    vuex is continue (actions)

--- vuex-app/src/App.vue
+++ vuex-app/src/App.vue
@@ -12,6 +12,7 @@
         <app-another-result></app-another-result>
         <hr>
         <app-counter></app-counter>
+        <app-another-counter></app-another-counter>
       </div>
     </div>
   </div>
@@ -21,6 +22,7 @@
   import Counter from './components/Counter.vue';
   import Result from './components/Result.vue';
   import AnotherResult from './components/AnotherResult.vue';
+  import AnotherCounter from './components/AnotherCounter.vue';
   export default {
     // data() {
     //   return {
@@ -30,7 +32,8 @@
     components:{
       appCounter: Counter,
       appResult: Result,
-      appAnotherResult: AnotherResult
+      appAnotherResult: AnotherResult,
+      appAnotherCounter: AnotherCounter
     }
   }
 </script>
